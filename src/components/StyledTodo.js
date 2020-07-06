25fc27ba168224d55fbb1dff2b3b8f864a83741b
Jessica <duellje@oregonstate.edu>
2020-07-06 18:04:29 -0400

    tweaked styling

--- src/components/StyledTodo.js
+++ src/components/StyledTodo.js
@@ -4,10 +4,22 @@ export const StyledButton = styled.button`
     height: 2rem;
     margin: 1%;
     font-size: 1rem;
-    background: #969696;
-    color: #F2ECFF;
+    background: black;
+    border: 2px solid #969696;
+    border-radius: 5px;
+    color: #C8FCEA;
 
     &:hover{
         cursor: pointer;
     }
+`
+
+export const StyledDiv = styled.div`
+    border: 1px solid #969696;
+    border-radius: 6px;
+    box-shadow: 7px 8px 25px -1px rgba(150,150,150,0.84);
+    margin: 3%;
+    background: black;
+    color: #C8FCEA;
+    width: 50%;
 `
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Todo.css';
+import {StyledDiv} from './StyledTodo'
 
 const Todo = props => {
     return(
-        <div 
+        <StyledDiv 
             onClick={() => {props.completeTask(props.task.id)}}
             className={`task${props.task.completed ? " completed" : ""}`}
         >
             <p>{props.task.task}</p>
-        </div>
+        </StyledDiv>
     )
 };
 
